8ad1e0df153c171b92f3e5d2d1268b513fecfb8c
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2022-06-08 14:40:04 +0200

    add analogRead

--- ESC-Brushless-Motor/ESC-Brushless-Motor.ino
+++ ESC-Brushless-Motor/ESC-Brushless-Motor.ino
@@ -4,18 +4,28 @@ Servo ESC2;
 Servo ESC3;
 Servo ESC4;
 
+int analogPin = A0;
+
+int val = -1;
+
 void setup() 
 {
   // Der ESC wird im Setup genauer definiert.
   ESC1.attach(8); // Die drei Werte bedeuten Pin, minimale Pulsweite und maximale Pulsweite in Mikrosekunden. 1000 und 2000 sind dabei Standartwerte, die sich fpr die meisten Speedcontroller eignen. Wenn es mit diesen Werten nicht funktioniert, müssen die Werte des verwendeten ESC recherchiert und eingesetzt werden.
-  ESC2.attach(9);
-  ESC3.attach(10);
-  ESC4.attach(11);
 }
 void loop() 
 {
-  ESC1.write(100);    // Der endgültige Wert für den ESC wird an den ESC gesendet. Der ESC nimmt das Signal an dieser Stelle auf und steuert den Motor entsprechend der gesendeten Werte an.
-  ESC2.write(100);
-  ESC3.write(100);
-  ESC4.write(100);
+  ESC1.write(0);
+
+  val = analogRead(analogPin);
+  Serial.println(val); 
+  
+  delay(5000);
+  
+  ESC1.write(100);
+
+  val = analogRead(analogPin);
+  Serial.println(val); 
+
+  delay(5000);
 }
