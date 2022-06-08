bc802c82ac9afe552496e7ae543effde253d55a2
KLukas04 <krinkelukas@gmail.com>
2022-06-08 18:44:16 +0200

    ES GEHT !!!

--- ESC-Brushless-Motor/ESC-Brushless-Motor.ino
+++ ESC-Brushless-Motor/ESC-Brushless-Motor.ino
@@ -15,20 +15,17 @@ void setup()
   
   // Der ESC wird im Setup genauer definiert.
   ESC1.attach(8); // Die drei Werte bedeuten Pin, minimale Pulsweite und maximale Pulsweite in Mikrosekunden. 1000 und 2000 sind dabei Standartwerte, die sich fpr die meisten Speedcontroller eignen. Wenn es mit diesen Werten nicht funktioniert, müssen die Werte des verwendeten ESC recherchiert und eingesetzt werden.
+  ESC1.write(0);
+  delay(5000);
 }
 void loop() 
 {
-  ESC1.write(0);
-
-  val = analogRead(analogPin);
-  Serial.println(val); 
   
-  delay(5000);
   
   ESC1.write(100);
 
-  val = analogRead(analogPin);
-  Serial.println(val); 
+  delay(10000);
 
-  delay(5000);
+  ESC1.write(50);
+  delay(10000);
 }
