e6bd384e54c108b07e4edbe0facbb91ea6b1d574
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2022-06-10 14:19:15 +0200

    adjusting speed via controller

--- Drone_V1/Drone_V1.ino
+++ Drone_V1/Drone_V1.ino
@@ -7,11 +7,16 @@
 #define PS2_CLK        12  //17
 byte error = 0;
 byte type = 0;
-
-PS2X ps2x; // create PS2 Controller Class
-
+PS2X ps2x;
 int motorSpeed = 0;
 byte vibration = 0;
+
+//ESC
+#include <Servo.h>
+
+Servo motA, motB, motC, motD;
+int mSpeed = 0;
+#define MAX_SPEED 60
 
 void setup() {
   Serial.begin(57600);
@@ -57,14 +62,73 @@ void setup() {
       break;
    }
 
+  //ESC
+  motA.attach(4, 1000, 2000); // 1000 and 2000 are very important ! Values can be different with other ESCs.
+  motB.attach(5, 1000, 2000);
+  motC.attach(6, 1000, 2000);
+  motD.attach(7, 1000, 2000);
+
 }
 
 void loop() {
-
-  //ESC-Calibration
   
   ps2x.read_gamepad(false, vibration);
 
+  //ESC-Calibration
+  //Stufe 1
+  if(ps2x.Button(PSB_L1)){
+      Serial.println("Sending 180 throttle");
+      motA.write(180);
+      motB.write(180);
+      motC.write(180);
+      motD.write(180);
+  }
+  //Hier Motor anschließen, Stufe 2
+  if(ps2x.Button(PSB_L2) || ps2x.Button(PSB_CIRCLE)){
+    Serial.println("Sending 0 throttle");
+    setAllZero();
+  }
+  //Stufe 3
+  if(ps2x.Button(PSB_TRIANGLE)){
+    
+    setAllZero();
+
+    for(int i = 0; i < 21; i++){
+        motA.write(i);
+        motB.write(i);
+        motC.write(i);
+        motD.write(i);
+        delay(200);
+    }
+    for(int i = 21; i > 0 ; i++){
+        motA.write(i);
+        motB.write(i);
+        motC.write(i);
+        motD.write(i);
+        delay(200);
+    }
+
+    setAllZero();
+    
+  }
+  
+
   int y = ps2x.Analog(PSS_LY);
 
+  if(y > 140){
+    if(mSpeed < MAX_SPEED){
+      mSpeed = mSpeed + 1;  
+    }  
+  }
+  else if(y < 116){
+    mSpeed = mSpeed - 1;  
+  }
+  
+}
+
+void setAllZero(){
+  motA.write(0);
+  motB.write(0);
+  motC.write(0);
+  motD.write(0);  
 }
