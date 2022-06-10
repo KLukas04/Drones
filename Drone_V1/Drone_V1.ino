bc50f9eddb0ae2e988b51ffe69c48e57151b19d4
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2022-06-10 14:02:47 +0200

    Create Drone_V1.ino

---
+++ Drone_V1/Drone_V1.ino
@@ -0,0 +1,63 @@
+//PS2 Controller
+#include <PS2X_lib.h>  //for v1.6
+
+#define PS2_DAT        13  //14    
+#define PS2_CMD        11  //15
+#define PS2_SEL        10  //16
+#define PS2_CLK        12  //17
+byte error = 0;
+
+PS2X ps2x; // create PS2 Controller Class
+
+int motorSpeed = 0;
+
+void setup() {
+  Serial.begin(57600);
+
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+
+  //ps2-Controller
+  
+  delay(300);  //added delay to give wireless ps2 module some time to startup, before configuring it
+  
+  //setup pins and settings: GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
+  error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, false, true);
+  
+  if(error == 0){
+    Serial.print("Found Controller, configured successful ");
+  }  
+  else if(error == 1)
+    Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
+   
+  else if(error == 2)
+    Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit www.billporter.info for troubleshooting tips");
+
+  else if(error == 3)
+    Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
+  
+  type = ps2x.readType(); 
+  
+  switch(type) {
+    case 0:
+      Serial.println("Unknown Controller type found ");
+      break;
+    case 1:
+      Serial.println("DualShock Controller found ");
+      break;
+    case 2:
+      Serial.println("GuitarHero Controller found ");
+      break;
+  case 3:
+      Serial.println("Wireless Sony DualShock Controller found ");
+      break;
+   }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
